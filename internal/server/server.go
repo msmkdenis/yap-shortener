57d58e71b55cddf6268322fc5e8e807b38ffb85f
msmkdenis <msmkdenis@gmail.com>
2023-10-18 16:07:45 +0300

    feat: added zap logger (request lvl)

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,10 +3,15 @@ package server
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/msmkdenis/yap-shortener/internal/handlers"
+	"github.com/msmkdenis/yap-shortener/internal/middleware"
+	"go.uber.org/zap"
 )
 
 func InitServer(URLServer string) {
+	logger, _ := zap.NewProduction()
+	requestLogger := middleware.InitRequestLogger(logger)
 	e := echo.New()
+	e.Use(requestLogger.RequestLogger())
 	e.POST("/", handlers.PostURL)
 	e.GET("/*", handlers.GetURL)
 	e.DELETE("/", handlers.DeleteAll)
