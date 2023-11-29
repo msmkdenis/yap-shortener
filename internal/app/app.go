1401e6e578de219fcda994fe350f90c1608b383b
Denis Burtsev <msmkdenis@gmail.com>
2023-11-29 18:14:01 +0300

    fix: cancel

--- internal/app/app.go
+++ internal/app/app.go
@@ -43,7 +43,8 @@ func URLShortenerRun() {
 		<-quit
 
 		// Shutdown signal with grace period of 30 seconds
-		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+		shutdownCtx, cancel := context.WithTimeout(serverCtx, 30*time.Second)
+		defer cancel()
 
 		go func() {
 			<-shutdownCtx.Done()
