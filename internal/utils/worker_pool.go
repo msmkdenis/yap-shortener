7b56647c90c5bfdd9397aeca2f01a1d1a41789b5
msmkdenis <msmkdenis@gmail.com>
2023-11-30 21:56:02 +0300

    fix: log errors in worker

--- internal/utils/worker_pool.go
+++ internal/utils/worker_pool.go
@@ -2,15 +2,12 @@ package utils
 
 import (
 	"go.uber.org/zap"
-	"sync"
 )
 
 type WorkerPool struct {
 	workers   int
 	taskQueue chan func() error
-	errChanel chan error
 	logger    *zap.Logger
-	wg        sync.WaitGroup
 }
 
 func NewWorkerPool(workers int, logger *zap.Logger) *WorkerPool {
@@ -18,13 +15,10 @@ func NewWorkerPool(workers int, logger *zap.Logger) *WorkerPool {
 		workers:   workers,
 		taskQueue: make(chan func() error),
 		logger:    logger,
-		errChanel: make(chan error),
-		wg:        sync.WaitGroup{},
 	}
 }
 
 func (wp *WorkerPool) Start() {
-	go wp.logError()
 	for i := 0; i < wp.workers; i++ {
 		go wp.runWorker()
 	}
@@ -34,22 +28,12 @@ func (wp *WorkerPool) runWorker() {
 	for task := range wp.taskQueue {
 		err := task()
 		if err != nil {
-			wp.errChanel <- err
+			wp.logger.Error("worker error", zap.Error(err))
 		}
-		wp.wg.Done()
 	}
-}
-
-func (wp *WorkerPool) logError() {
-	for err := range wp.errChanel {
-		wp.logger.Error("worker error", zap.Error(err))
-	}
-	wp.wg.Wait() // wait for all workers to finish & and all errors to be logged
-	close(wp.errChanel)
 }
 
 func (wp *WorkerPool) Submit(task func() error) {
-	wp.wg.Add(1)
 	wp.taskQueue <- task
 }
 
