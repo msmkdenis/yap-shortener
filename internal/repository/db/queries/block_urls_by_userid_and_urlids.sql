b635b867600dfa283802bcc8f246277f2a91bb04
Denis Burtsev <msmkdenis@gmail.com>
2024-01-21 16:04:47 +0300

    iter16

--- internal/repository/db/queries/block_urls_by_userid_and_urlids.sql
+++ internal/repository/db/queries/block_urls_by_userid_and_urlids.sql
@@ -1,1 +1,3 @@
-select id, user_id, deleted_flag from url_shortener.url where user_id = $1 and id = $2 for update
+select id, user_id, deleted_flag
+from url_shortener.url
+where user_id = $1 and id = $2 for update
