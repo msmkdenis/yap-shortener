5a8ce68b5c6acfe5dabb785b380214b3082d173c
Denis Burtsev <msmkdenis@gmail.com>
2024-02-07 14:15:11 +0300

    style: global build info variables

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "net/http/pprof"
 
 	"github.com/msmkdenis/yap-shortener/internal/app/shortener"
 )
 
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+)
+
+func printGreeting() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n\n", buildCommit)
+}
+
 func main() {
+	printGreeting()
 	shortener.URLShortenerRun()
 }
